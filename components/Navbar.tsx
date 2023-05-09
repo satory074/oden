1840b597420c6ad9f92a9a5d7c533d00778050f9
satory074 <satory074@gmail.com>
2023-05-10 00:59:26 +0900

    update: navbar scss

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import styles from "../styles/navbar.module.scss";
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className={styles.navbar}>
             <ul>
                 <li>
                     <Link href="/">
