313eb19ed48e9e7c4b8c287b1d5aae573bb7ca06
satory074 <satory074@gmail.com>
2023-06-24 11:19:41 +0900

    feat: Google analytics - refs # #17

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 import "github-markdown-css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { GA_TRACKING_ID, pageview } from "../lib/gtag";
 import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!GA_TRACKING_ID) {
+            return;
+        }
+
+        const handleRouteChange = (path: string) => {
+            pageview(path);
+        };
+
+        router.events.on("routeChangeComplete", handleRouteChange);
+
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
     return <Component {...pageProps} />;
 }
