5ad23a02fbe73444170c60e9a996dd45d5ffe7e5
satory074 <satory074@gmail.com>
2023-06-27 07:36:27 +0900

    add: styles to MainContent component - refs #36

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,7 +1,17 @@
+import styles from "../styles/MainContent.module.scss";
+
 const MainContent = () => {
     return (
-        <div>
-            <h1>My Blog</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>My Blog</h1>
+            <p className={styles.description}>This is my blog.</p>
+
+            <h2 className={styles.subtitle}>Github</h2>
+            <a href="https://github.com/satory074" target="_blank">
+                <img src="https://grass-graph.appspot.com/images/satory074.png" />
+            </a>
+
+            <h2 className={styles.subtitle}></h2>
         </div>
     );
 };
